8fabd60b1ffb5aca9244e28055d64e7263e3683f
Carter Jin <carterjin@Carters-MacBook-Pro.local>
2020-06-27 12:04:09 -0400

    eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['airbnb-typescript-prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/self-closing-comp': 'off',
+    'prettier/prettier': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 };
