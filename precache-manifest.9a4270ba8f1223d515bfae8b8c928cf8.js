569ddfb2a46383fa7be4677e7cb9e8641a2020c2
Carter Jin <carterjin@Carters-MacBook-Pro.local>
2020-08-09 20:22:07 -0400

    Updates

---
+++ precache-manifest.9a4270ba8f1223d515bfae8b8c928cf8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705f2c5f829bb1820b515856bd0813fc",
+    "url": "/Match-em/index.html"
+  },
+  {
+    "revision": "ea1c7e88a577a786f646",
+    "url": "/Match-em/static/js/2.7d212d6e.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/Match-em/static/js/2.7d212d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfd26ecb82e41a5fbdb",
+    "url": "/Match-em/static/js/main.5310fef7.chunk.js"
+  },
+  {
+    "revision": "73a967f8921d2d5ebe51",
+    "url": "/Match-em/static/js/runtime-main.a4b22381.js"
+  },
+  {
+    "revision": "98bcf95154f73f06d01244245f0e0b1d",
+    "url": "/Match-em/static/media/Alice2.98bcf951.png"
+  },
+  {
+    "revision": "be6109be55ca81558b9ad2dea4722779",
+    "url": "/Match-em/static/media/Alice2.be6109be.png"
+  },
+  {
+    "revision": "64869c8d74cc7f6cfaefd39631159fcb",
+    "url": "/Match-em/static/media/Eugeo3.64869c8d.png"
+  },
+  {
+    "revision": "9dc9f2d6428a31caa3fe748bd21ad0d6",
+    "url": "/Match-em/static/media/Eugeo3.9dc9f2d6.png"
+  },
+  {
+    "revision": "47798d2c0a36958f6233abfc49f49517",
+    "url": "/Match-em/static/media/a-sinon.47798d2c.png"
+  },
+  {
+    "revision": "abbe6d3f2b0f8410a66d1c490b8d62b0",
+    "url": "/Match-em/static/media/a-sinon.abbe6d3f.png"
+  },
+  {
+    "revision": "4b0807a784d34d54bee16ce28dbaa17b",
+    "url": "/Match-em/static/media/b-leafa.4b0807a7.png"
+  },
+  {
+    "revision": "c6ad05f48a49b51319eb252220ebff63",
+    "url": "/Match-em/static/media/b-leafa.c6ad05f4.png"
+  },
+  {
+    "revision": "49be59bda1e511c6693d9f4a293e974e",
+    "url": "/Match-em/static/media/controller.49be59bd.png"
+  },
+  {
+    "revision": "564684437be79b6ed6474dbe00d3b940",
+    "url": "/Match-em/static/media/diamond.56468443.png"
+  },
+  {
+    "revision": "70f3a25db9acb9d7b223431ca2e9939d",
+    "url": "/Match-em/static/media/onsen-silica.70f3a25d.png"
+  },
+  {
+    "revision": "cba6ab132de4bb0c5fa44205931916a4",
+    "url": "/Match-em/static/media/onsen-silica.cba6ab13.png"
+  },
+  {
+    "revision": "7d9c44e640d37c09ab55b0a053501dd4",
+    "url": "/Match-em/static/media/os-asuna.7d9c44e6.png"
+  },
+  {
+    "revision": "8b26aecf1021edf5214edee3f5f9690e",
+    "url": "/Match-em/static/media/osasuna.8b26aecf.png"
+  },
+  {
+    "revision": "27ca5590dcededee6290e84e06da21cd",
+    "url": "/Match-em/static/media/rainy-rain.27ca5590.png"
+  },
+  {
+    "revision": "bf77189a53df40a7fc05765f946b9e98",
+    "url": "/Match-em/static/media/rainy-rain.bf77189a.png"
+  },
+  {
+    "revision": "286f40a0ee2589e9d449cd0fb3da59f4",
+    "url": "/Match-em/static/media/sport-yui.286f40a0.png"
+  },
+  {
+    "revision": "86c4f5f8214ff1458f0f565fa0dda738",
+    "url": "/Match-em/static/media/sport-yui.86c4f5f8.png"
+  },
+  {
+    "revision": "6c8e694d6aeca4275714288442c751ca",
+    "url": "/Match-em/static/media/title.6c8e694d.png"
+  },
+  {
+    "revision": "4db57afde8cf2f7ec9d77ecb355a3fa5",
+    "url": "/Match-em/static/media/w-kirito.4db57afd.png"
+  },
+  {
+    "revision": "d27be3831ae6509ee28ed36881696175",
+    "url": "/Match-em/static/media/w-kirito.d27be383.png"
+  }
+]);
